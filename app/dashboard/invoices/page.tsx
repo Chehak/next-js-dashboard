2b44ac2f41cbf377ab6fef9567a23f5a04d6bb8f
Chehak Gupta <gchehak18@gmail.com>
2024-06-20 17:35:52 +0530

    table- issue

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -4,7 +4,8 @@ import { CreateInvoice } from '@/app/ui/invoices/buttons';
 import { lusitana } from '@/app/ui/fonts';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
- 
+import Table from '@/app/ui/invoices/table';
+
 export default async function Page({searchParams,}:{
   searchParams?:{
     query?:string;
