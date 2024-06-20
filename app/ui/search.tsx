ce56789db51136fc45a96656b5c830eb3ecec335
Chehak Gupta <gchehak18@gmail.com>
2024-06-20 16:02:07 +0530

    search params

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -6,21 +6,17 @@ import { useSearchParams , usePathname , useRouter } from "next/navigation";
 export default function Search({ placeholder }: { placeholder: string }) {
   const params = useSearchParams();
   const pathName = usePathname();
-  const {replace} = useRouter()
+  const { replace } = useRouter()
   function handleOnchange(val:any){
     const searchParams = new URLSearchParams(params);
     if(val){
-      searchParams.set('query',val)
+      searchParams.set('query',val);
     }
     else{
-      searchParams.delete('query')
+      searchParams.delete('query');
     }
-
-    replace(`${pathName}?${params.toString()}`);
-
-    
+    replace(`${pathName}?${searchParams.toString()}`);
   }
-
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
@@ -31,6 +27,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
         onChange={(e)=>{handleOnchange(e.target.value)}}
+        defaultValue = {params.get('query')?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
