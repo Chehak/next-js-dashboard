82be5c68b2dee8a06f798d8798a14a9e8333fb51
chehak <gchehak18@gmail.com>
2024-06-24 22:20:31 +0530

    add invoices and redirect complete

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,10 +1,10 @@
 'use server';
 
 import { z } from 'zod';
-import {sql} from '@vercel/postgres'
-import {revalidatePath} from 'next/cache'
-import {redirect} from 'next/navigation'
- 
+import { sql } from '@vercel/postgres';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+
 const FormSchema = z.object({
   id: z.string(),
   customerId: z.string(),
@@ -12,9 +12,9 @@ const FormSchema = z.object({
   status: z.enum(['pending', 'paid']),
   date: z.string(),
 });
- 
+
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
- 
+
 export async function createInvoice(formData: FormData) {
   const { customerId, amount, status } = CreateInvoice.parse({
     customerId: formData.get('customerId'),
@@ -25,11 +25,10 @@ export async function createInvoice(formData: FormData) {
   const date = new Date().toISOString().split('T')[0];
 
   await sql`
-  INSERT INTO invoices(customerId, amount, status,date)
-  VALUES (${customerId}, ${amount} , ${status} , ${date})
+  INSERT INTO invoices(customer_id, amount, status,date)
+  VALUES (${customerId}, ${amountInCents} , ${status} , ${date})
   `;
 
-  revalidatePath('/dashboard/invoices')
-  redirect('/dashboard/invoices')
-
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
 }
